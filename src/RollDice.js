ba3adda9f7dfff0fcc9a520a7cfe71a7c3090c0a
oguzyurek <105684710+oguzyurek@users.noreply.github.com>
2023-03-11 23:30:49 -0500

    Add disabled button

--- src/RollDice.js
+++ src/RollDice.js
@@ -11,6 +11,7 @@ class RollDice extends Component {
     this.state = {
       dice1: "one",
       dice2: "two",
+      rolling: false,
     };
   }
   roll() {
@@ -18,17 +19,24 @@ class RollDice extends Component {
       this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
     const newDice2 =
       this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
-    this.setState({ dice1: newDice1, dice2: newDice2 });
+    this.setState({ dice1: newDice1, dice2: newDice2, rolling: true });
     console.log(`Dice 1 is:${newDice1}`);
     console.log(`Dice 2 is:${newDice2}`);
+
+    setTimeout(() => {
+      this.setState({ rolling: false });
+    }, 1000);
   }
   render() {
     return (
       <div>
-        <Dice face={this.state.dice1} />
-        <Dice face={this.state.dice2} />
-
-        <button onClick={this.roll}>Roll Dice!</button>
+        <div className="dices">
+          <Dice face={this.state.dice1} />
+          <Dice face={this.state.dice2} />
+        </div>
+        <button onClick={this.roll} disabled={this.state.rolling}>
+          {this.state.rolling ? "Rolling..." : "Roll Dice!"}
+        </button>
       </div>
     );
   }
